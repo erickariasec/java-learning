7d29d83b3f3609d2b62d31806a23a473458380eb
Erick Arias <erickarias1999@hotmail.com>
2021-09-06 19:29:54 -0500

    Chapter 1, Declaraciones Operadores

---
+++ 1.PrimerosPasos/src/Declaraciones_Operadores.java
@@ -0,0 +1,23 @@
+
+public class Declaraciones_Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//int a=5;
+		//int b;
+		double a=5;
+		double b;
+		
+		b=7;
+		
+		double c=b/a;
+		
+		c+=6;
+		//c-=6;
+		
+		System.out.println(c);
+
+	}
+
+}
