53de357323a0d786b4303f3755352b5842e5179f
Erick Arias <erickarias1999@hotmail.com>
2021-09-07 12:15:31 -0500

    CHAPTER 1, Calculations Using Math Class

---
+++ CHAPTER-1-FirstSteps/src/CalculationsUsingMath.java
@@ -0,0 +1,13 @@
+
+public class CalculationsUsingMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double raiz=Math.sqrt(9);	// Math Class for Square Root 
+		
+		System.out.println(raiz);
+
+	}
+
+}
