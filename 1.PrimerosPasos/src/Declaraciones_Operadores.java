08dcb282518ce0e3c62f0321531479def6d7408a
Erick Arias <erickarias1999@hotmail.com>
2021-09-07 10:16:21 -0500

    Chapter 1, Declarations and Operations, Second Part

--- 1.PrimerosPasos/src/Declaraciones_Operadores.java
+++ 1.PrimerosPasos/src/Declaraciones_Operadores.java
@@ -4,6 +4,9 @@ public class Declaraciones_Operadores {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// FIRST PART
+		/*
+		
 		//int a=5;
 		//int b;
 		double a=5;
@@ -17,7 +20,20 @@ public class Declaraciones_Operadores {
 		//c-=6;
 		
 		System.out.println(c);
+		
+		*/
 
+		
+		// SECOND PART
+		
+		final double apulgadas=2.54;		// CONSTANTES
+		
+		double cm=6;
+		
+		double resultado=cm/apulgadas;
+		
+		System.out.println("En " + cm + " cm hay " + resultado + " pulgadas.");
+		
 	}
 
 }
