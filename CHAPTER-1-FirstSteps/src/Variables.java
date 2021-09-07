064a40cabaf4b124e6076e828f6a1ddde60366c4
Erick Arias <erickarias1999@hotmail.com>
2021-09-07 11:06:32 -0500

    CHAPTER 1, Variables

---
+++ CHAPTER-1-FirstSteps/src/Variables.java
@@ -0,0 +1,21 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		byte edad;
+		
+		edad=35;	// Declaración e iniciación de variable en una única línea
+		
+		/* La siguiente instrucción imprime en la consola (System.out) el
+		 * valor de la variable edad
+		 */
+		System.out.println(edad);
+		
+		edad=75;
+		
+		System.out.println(edad);
+
+	}
+
+}
