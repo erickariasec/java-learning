53df465e1383d6ced64f12481ad4afa89417eec3
Erick Arias <erickarias1999@hotmail.com>
2021-09-13 12:53:35 -0500

    CHAPTER 1, Manipulate Strings, Method length and charAt

--- CHAPTER-1-FirstSteps/src/ManipulateStrings.java
+++ CHAPTER-1-FirstSteps/src/ManipulateStrings.java
@@ -12,8 +12,9 @@ public class ManipulateStrings {
 		
 		System.out.println("The first letter of my name is " + name.charAt(0));
 		
+		// Three ways to find last letter of a String
 		int lastLetter=name.length();
-		System.out.println("The last letter of " + name + " is " + name.charAt(4) + ".");	// Available only for "Erick" or other words of 5 letters.
+		System.out.println("The last letter of " + name + " is " + name.charAt(4) + ".");	// Available only for "Erick" or other words of 5 letters. (This is not the best way)
 		System.out.println("The last letter of " + name + " is " + name.charAt(lastLetter - 1) + ".");	// Available for all words, it doesn´t matter the word long.
 		System.out.println("The last letter of " + name + " is " + name.charAt(name.length() - 1) + ".");	// Another method available for all words. (From Youtube comments)
 	}
