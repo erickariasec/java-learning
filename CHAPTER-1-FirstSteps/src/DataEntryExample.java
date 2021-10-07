97ea41d79c1b5eca466afc7ee21f607d25bfc801
Erick Arias <erickarias1999@hotmail.com>
2021-10-07 12:04:49 -0500

    CHAPTER 1, Data Entry Example, Scanner use

--- CHAPTER-1-FirstSteps/src/DataEntryExample.java
+++ CHAPTER-1-FirstSteps/src/DataEntryExample.java
@@ -4,14 +4,18 @@ public class DataEntryExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner dataEntry=new Scanner(System.in);
+		
+		// CREATE SCANNER OBJECT
+		Scanner dataEntry=new Scanner(System.in); 
 		
 		System.out.println("Enter your name, please:");
 		
+		// SCAN USER NAME
 		String userName=dataEntry.nextLine();
 		
 		System.out.println("Enter your age, please:");
 		
+		// SCAN USER AGE
 		int userAge=dataEntry.nextInt();
 		
 		System.out.println();
