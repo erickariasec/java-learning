a503e4aa3629a79cebdc05114c9ea6e01018f8d7
Erick Arias <erickarias1999@hotmail.com>
2021-10-08 10:12:05 -0500

    CHAPTER 1, Numbers Entry Example, Double.parseDouble(String s), print, println, printf

---
+++ CHAPTER-1-FirstSteps/src/NumbersEntryExample.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+public class NumbersEntryExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double x=10000.0;
+		
+		System.out.println(x/3); // Print all point values
+		
+		System.out.printf("%1.2f", (x/3)); // Print only values designed
+		
+		System.out.println();
+		System.out.println();
+		
+		
+		
+		
+		// SCAN USER NUMBER AND CALCULATE SQUARE ROOT
+		System.out.println("LET´S CALCULATE SQUARE ROOT");
+		
+		String userNumber=JOptionPane.showInputDialog("Enter a number, please");
+		
+		// CONVERSION FROM STRING TO DOUBLE
+		double convertedUserNumber=Double.parseDouble(userNumber);
+		
+		System.out.println(); // Add new line
+		System.out.print("The square root of " + convertedUserNumber + " is "); // Doesn´t add new line 
+		System.out.printf("%1.2f", (Math.sqrt(convertedUserNumber))); // Formatted output form
+
+	}
+
+}
