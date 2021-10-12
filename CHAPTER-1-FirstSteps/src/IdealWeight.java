e6a3b6211cb03a184bcd6aa242cfff442ba69971
Erick Arias <erickarias1999@hotmail.com>
2021-10-12 10:01:44 -0500

    CHAPTER 1, Ideal Weight, do while

--- CHAPTER-1-FirstSteps/src/IdealWeight.java
+++ CHAPTER-1-FirstSteps/src/IdealWeight.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class IdealWeight {
+import javax.swing.*; // This package contains JOptionPane Class and showInputDialog Method
+
+public class IdealWeight {
+    public static void main(String[] args) {
+
+        String genre="";
+
+        do{
+            genre=JOptionPane.showInputDialog("Enter your genre H/M");
+        } while (!genre.equalsIgnoreCase("H") && !genre.equalsIgnoreCase("M"));
+       // while (genre.equalsIgnoreCase("H")==false && genre.equalsIgnoreCase("M")==false);
+
+        int height=Integer.parseInt(JOptionPane.showInputDialog("Enter your HEIGHT in cm"));
+        int idealWeight=0;
+
+        // FOR MAN
+        if (genre.equalsIgnoreCase("H")){
+            idealWeight=(height-110);
+        }
+
+        // FOR WOMAN
+        else if (genre.equalsIgnoreCase("M")){
+            idealWeight=(height-120);
+        }
+
+        System.out.println("Your ideal weight is " + idealWeight + " kg!");
+    }
 }
