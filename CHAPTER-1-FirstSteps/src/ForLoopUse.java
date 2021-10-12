5e72b4f76f58623db2384687594661cfd0e1eb04
Erick Arias <erickarias1999@hotmail.com>
2021-10-12 10:27:32 -0500

    CHAPTER 1, For Loop Use, simple example

---
+++ CHAPTER-1-FirstSteps/src/ForLoopUse.java
@@ -0,0 +1,12 @@
+public class ForLoopUse {
+    public static void main(String[] args) {
+
+        for (int i=0; i<10; i++){
+
+            // Prints "Erick" 10 times
+            System.out.println("Erick");
+
+        }
+
+    }
+}
