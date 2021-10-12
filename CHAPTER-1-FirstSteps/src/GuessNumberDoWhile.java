d9262f6b06b5fde4d59267004a38bcfbad7ea09a
Erick Arias <erickarias1999@hotmail.com>
2021-10-12 09:59:05 -0500

    CHAPTER 1, Guess Number, do while loop

--- CHAPTER-1-FirstSteps/src/GuessNumberDoWhile.java
+++ CHAPTER-1-FirstSteps/src/GuessNumberDoWhile.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class GuessNumberDoWhile {
+import java.util.*;
+
+public class GuessNumberDoWhile {
+    public static void main(String[] args) {
+
+        int generateRandom=(int)(Math.random()*100); // REFUNDING
+
+        Scanner dataEntry=new Scanner(System.in);
+
+        // INITIALIZE VARIABLES
+        int userNumber=0;
+        int attempts=0;
+
+        // BUCLE DO WHILE
+        do{
+            attempts++; // Attempts number increment each time you enter the while loop
+
+            System.out.println("Enter a number, please:");
+            userNumber= dataEntry.nextInt();
+
+            if (generateRandom<userNumber){
+                System.out.println("\nLower!\n");
+            }
+            else if (generateRandom>userNumber){
+                System.out.println("\nHigher!\n");
+            }
+        } while (generateRandom!=userNumber);
+
+        System.out.println("\n\n****CONGRATULATIONS!**** \n\t\tYOU WIN! \n\nYOU GOT IT " +
+                "IN " + attempts + " ATTEMPTS!");
+
+    }
 }
