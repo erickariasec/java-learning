496ba183a51ac99b8b2294bf19aa8c4a4ec156f6
Erick Arias <erickarias1999@hotmail.com>
2021-10-12 16:33:10 -0500

    CHAPTER 1, Email Check, Bucle for, length( ) and charAt(i) Methods, boolean

--- CHAPTER-1-FirstSteps/src/EmailCheck.java
+++ CHAPTER-1-FirstSteps/src/EmailCheck.java
@@ -3,7 +3,13 @@ import javax.swing.*;
 public class EmailCheck {
     public static void main(String[] args) {
 
-        boolean arroba=false;
+        int arroba=0;
+        boolean dot=false;
+
+        /* Remember that Java does not erase information of bucles, Java keep that
+        * information for the next loop. For that reason, if at first time you entered
+        * a dot, Java will detect and keep that dot for the next loop although in the
+        * next loop you do not enter any dot. */
 
         do{
             String userEmail= JOptionPane.showInputDialog("Enter your email, please");
@@ -11,18 +17,20 @@ public class EmailCheck {
             for (int i=0; i<userEmail.length(); i++){
 
                 if (userEmail.charAt(i)=='@'){
-                    arroba=true;
-                    break;
+                    arroba=1;
+                }
+                if (userEmail.charAt(i)=='.'){
+                    dot=true;
                 }
             }
 
-            if (arroba==true){
+            if (arroba==1 && dot==true){
                 System.out.println("\n\n***Email entered is CORRECT!***");
             }
             else {
                 System.out.println("\nINCORRECT EMAIL, TRY AGAIN!");
             }
-        } while (arroba==false);
+        } while (arroba==0 || dot==false);
 
     }
 }
