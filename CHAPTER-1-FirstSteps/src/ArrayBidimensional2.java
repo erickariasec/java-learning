8f5e9dac087da3b1d4095971f51097e718f94477
Erick Arias <erickarias1999@hotmail.com>
2021-10-18 12:06:55 -0500

    CHAPTER 1, Array Bidimensional 2, Another way, Nested for each loop

--- CHAPTER-1-FirstSteps/src/ArrayBidimensional2.java
+++ CHAPTER-1-FirstSteps/src/ArrayBidimensional2.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class ArrayBidimensional2 {
+public class ArrayBidimensional2 {
+    public static void main(String[] args) {
+
+        int [][] arrayBi={
+                {15, 21, 18, 9, 15},
+                {10, 52, 17, 19, 7},
+                {19, 23, 19, 17, 7},
+                {92, 13, 13, 32, 41}
+        };
+
+        for(int[]fila:arrayBi){
+            System.out.println();
+            for(int z:fila){
+                System.out.print(z + " ");
+            }
+
+        }
+    }
 }
