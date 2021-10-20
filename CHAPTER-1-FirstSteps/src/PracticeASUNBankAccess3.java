952ab5311aa1bface3160a86ab64d8928068890d
Erick Arias <erickarias1999@hotmail.com>
2021-10-20 16:33:00 -0500

    CHAPTER 1, Practice ASUN Before POO, Bank Access 3, v2.0

--- CHAPTER-1-FirstSteps/src/PracticeASUNBankAccess3.java
+++ CHAPTER-1-FirstSteps/src/PracticeASUNBankAccess3.java
@@ -15,10 +15,11 @@ public class PracticeASUNBankAccess3 { // Practice All Seen Until Now - Bank Acc
         int arroba=0;   // for Email
         boolean dot=false;  // for Email
         int ID;     // for ID Number
+        int signIn;     // for Sign In
 
         // Full Name Option
-        String userCompleteName=JOptionPane.showInputDialog("Enter your full name");
-        System.out.println(userCompleteName);
+        String userFullName=JOptionPane.showInputDialog("Enter your full name");
+        System.out.println("***FULL NAME REGISTERED CORRECTLY***");
 
         // ID Number Option
         do{
@@ -57,6 +58,7 @@ public class PracticeASUNBankAccess3 { // Practice All Seen Until Now - Bank Acc
         // Password Option
         String userPassword= JOptionPane.showInputDialog("Enter your new password");
         System.out.print("PASSWORD SAVED CORRECTLY\n\n\n");
+        JOptionPane.showMessageDialog(null, "SUCCESSFUL REGISTRATION"); // extracted from Google
 
         // COMPLETE REGISTRATION
         System.out.println("***********************************");
@@ -64,6 +66,27 @@ public class PracticeASUNBankAccess3 { // Practice All Seen Until Now - Bank Acc
         System.out.println("***** SUCCESSFUL REGISTRATION *****");
         System.out.println("***********************************");
         System.out.println("***********************************");
+
+
+        // SIGN IN MENU
+        JOptionPane.showMessageDialog(null, "TO VALIDATE YOUR DATA, SIGN IN, PLEASE"); // Google
+        do{
+            System.out.print("\n\n\n***SIGN IN***\n\n");
+            String registeredFullName=JOptionPane.showInputDialog("Enter your full name");
+            String registeredUserPassword=JOptionPane.showInputDialog("Enter your password");
+            if((registeredFullName.equals(userFullName)) && (registeredUserPassword.equals(userPassword))){
+                System.out.print("\n\n***ACCESS ALLOWED***\n\n");
+                signIn=1;
+            }
+            else{
+                System.out.print("\n\n***INVALID NAME OR PASSWORD***\n");
+                System.out.print("**********TRY  AGAIN**********\n\n");
+                signIn=0;
+            }
+        } while (signIn==0);
+
+        System.out.println("Welcome " + userFullName + " it´s a pleasure!");
+        System.out.println("Thanks for being part of our Association of Savings and Credits");
 
     }
 }
