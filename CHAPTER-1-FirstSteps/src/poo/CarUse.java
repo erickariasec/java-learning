81c4c50b5fcab92788192ef6fdbc3c5f8e26f221
Erick Arias <erickarias1999@hotmail.com>
2021-10-23 10:07:07 -0500

    CHAPTER 1, POO, Car Use, Instantiate a Class

---
+++ CHAPTER-1-FirstSteps/src/poo/CarUse.java
@@ -0,0 +1,12 @@
+package poo;
+
+public class CarUse {
+
+    public static void main(String[] args) {
+
+        Car Renault=new Car();      // INSTANTIATE A CLASS. CLASS EXEMPLARY
+
+        System.out.println("This car has " + Renault.wheels + " wheels.");
+
+    }
+}
