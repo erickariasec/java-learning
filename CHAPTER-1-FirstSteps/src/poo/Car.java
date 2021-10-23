5ae66f6e1ec74969ecfed4521f63b3ad22a73e0a
Erick Arias <erickarias1999@hotmail.com>
2021-10-23 09:47:21 -0500

    CHAPTER 1, poo Package Creation, Class Car, Constructor Method

---
+++ CHAPTER-1-FirstSteps/src/poo/Car.java
@@ -0,0 +1,23 @@
+package poo;
+
+public class Car {
+
+    int wheels;
+    int length;
+    int width;
+    int engine;
+    int weight;
+
+
+
+    public Car(){           // CONSTRUCTOR METHOD
+
+        wheels=4;
+        length=2000;
+        width=300;
+        engine=1600;
+        weight=500;
+
+    }
+
+}
