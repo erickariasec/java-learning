6c07f971c7a032aed08d95c5adfce4b688055b10
Erick Arias <erickarias1999@hotmail.com>
2021-10-25 12:08:26 -0500

    CHAPTER 1, POO, Employee Use, GregorianCalendar Class, getTime Method, SETTERS and GETTERS, all in a single file

--- CHAPTER-1-FirstSteps/src/poo/EmployeeUse.java
+++ CHAPTER-1-FirstSteps/src/poo/EmployeeUse.java
@@ -12,6 +12,27 @@ class Employee{
 
     public Employee(String nam, double sal, int year, int month, int day){  // This CONSTRUCTOR method receives PARAMETERS/ARGUMENTS
 
+        name=nam;
+        salary=sal;
+        GregorianCalendar calendar=new GregorianCalendar(year, month-1, day);   // CLASS: GregorianCalendar - January=0
+        dischargeDateContract=calendar.getTime();   // METHOD: getTime (Inherited Mehtod)
+    }
+
+    public String returnName(){     // GETTER
+        return name;
+    }
+
+    public double returnSalary(){   // GETTER
+        return salary;
+    }
+
+    public Date returnDischargeDateContract(){  // GETTER
+        return dischargeDateContract;
+    }
+
+    public void raiseSalary(double percentage){     // SETTER
+        double salaryIncrease=salary*percentage/100;
+        salary+=salaryIncrease;
     }
 
     private String name;
