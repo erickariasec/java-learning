d11713e374aed51095d9e9c0bc998462103b1a18
Erick Arias <erickarias1999@hotmail.com>
2021-10-25 13:03:17 -0500

    CHAPTER 1, POO, Employee Use, Create an Array from Object/Class, Instantiate a Class, Bucle For, Bucle For Each

--- CHAPTER-1-FirstSteps/src/poo/EmployeeUse.java
+++ CHAPTER-1-FirstSteps/src/poo/EmployeeUse.java
@@ -5,6 +5,47 @@ import java.util.*;
 public class EmployeeUse {
     public static void main(String[] args) {
 
+        // THE LONGEST WAY
+        /*Employee Employee1=new Employee("Stephan Hidalgo", 85000, 1990, 12, 17);
+        Employee Employee2=new Employee("Erick Brooks", 95000, 1995, 6, 2);
+        Employee Employee3=new Employee("Michell Heralds", 105000, 2002, 3, 15);
+
+        Employee1.raiseSalary(5);
+        Employee2.raiseSalary(5);
+        Employee3.raiseSalary(5);
+
+        System.out.println("Name: " + Employee1.returnName() + " Salary: " + Employee1.returnSalary() + " Discharge Date: " + Employee1.returnDischargeDateContract());
+        System.out.println("Name: " + Employee2.returnName() + " Salary: " + Employee2.returnSalary() + " Discharge Date: " + Employee2.returnDischargeDateContract());
+        System.out.println("Name: " + Employee3.returnName() + " Salary: " + Employee3.returnSalary() + " Discharge Date: " + Employee3.returnDischargeDateContract());*/
+
+        Employee[] myEmployees=new Employee[3];
+
+        myEmployees[0]=new Employee("Stephan Hidalgo", 85000, 1990, 12, 17);
+        myEmployees[1]=new Employee("Erick Brooks", 95000, 1995, 6, 2);
+        myEmployees[2]=new Employee("Michell Heralds", 105000, 2002, 3, 15);
+
+        // BUCLE FOR (Normal)
+        /*for(int i=0; i<3; i++){
+            myEmployees[i].raiseSalary(5);
+        }*/
+
+        // BUCLE FOR EACH (Improved)
+        for(Employee i: myEmployees){
+            i.raiseSalary(5);
+        }
+
+        // BUCLE FOR (Normal)
+        /*for(int i=0; i<3; i++){
+            System.out.println("Name: " + myEmployees[i].returnName() + " Salary: " + myEmployees[i].returnSalary() +
+                    " Discharge Date: " + myEmployees[i].returnDischargeDateContract());
+        }*/
+
+        // BUCLE FOR EACH (Improved)
+        for(Employee i: myEmployees){
+            System.out.println("Name: " + i.returnName() + " Salary: " + i.returnSalary() +
+                    " Discharge Date: " + i.returnDischargeDateContract());
+        }
+
     }
 }
 
