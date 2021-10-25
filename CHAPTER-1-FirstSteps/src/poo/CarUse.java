3171a3a92529be5c877e7e0c91aa04a8e8e847fb
Erick Arias <erickarias1999@hotmail.com>
2021-10-25 10:06:10 -0500

    CHAPTER 1, POO, Car Use, Establishing different parameters using JOptionPane

--- CHAPTER-1-FirstSteps/src/poo/CarUse.java
+++ CHAPTER-1-FirstSteps/src/poo/CarUse.java
@@ -1,20 +1,22 @@
 package poo;
 
+import javax.swing.*;
+
 public class CarUse {
 
     public static void main(String[] args) {
 
         Car Renault=new Car();      // INSTANTIATE A CLASS. CLASS EXEMPLARY
 
-        Renault.set_color("blue");        // Call SETTER / PARAMETER PASSING
+        Renault.set_color(JOptionPane.showInputDialog("Enter color"));        // Call SETTER / PARAMETER PASSING
 
         System.out.println(Renault.return_generalData());
         System.out.println(Renault.return_color());
 
-        Renault.configure_seats("no");
+        Renault.configure_seats(JOptionPane.showInputDialog("Do you want leather seats? (yes/no)"));
         System.out.println(Renault.return_seats());
 
-        Renault.configure_airConditioning("yes");
+        Renault.configure_airConditioning(JOptionPane.showInputDialog("Do you want air conditioning? (yes/no)"));
         System.out.println(Renault.return_airConditioning());
 
         System.out.println(Renault.return_weightCar() + " kg");
