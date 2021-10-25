00a675a641bce418aa50cf1b6b5f587ca1aaa951
Erick Arias <erickarias1999@hotmail.com>
2021-10-25 11:19:24 -0500

    CHAPTER 1, POO, Employee Use, a single file will hold several classes, CONSTRUCTOR method receives PARAMETERS/ARGUMENTS

---
+++ CHAPTER-1-FirstSteps/src/poo/EmployeeUse.java
@@ -0,0 +1,20 @@
+package poo;
+
+import java.util.*;
+
+public class EmployeeUse {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Employee{
+
+    public Employee(String nam, double sal, int year, int month, int day){  // This CONSTRUCTOR method receives PARAMETERS/ARGUMENTS
+
+    }
+
+    private String name;
+    private double salary;
+    private Date dischargeDateContract;
+}
