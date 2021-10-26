f67eed79f31856e24e4734bf34cb398f5d6b0de3
Erick Arias <erickarias1999@hotmail.com>
2021-10-26 10:24:57 -0500

    CHAPTER 1, POO, Test Constant, STATIC method

--- CHAPTER-1-FirstSteps/src/poo/TestConstant.java
+++ CHAPTER-1-FirstSteps/src/poo/TestConstant.java
@@ -7,6 +7,8 @@ public class TestConstant {
         // Step 6: Instantiate EmployeeTest Class / Create objects belonging to EmployeeTest Class
         EmployeesTest Employee1= new EmployeesTest("Juan");
         EmployeesTest Employee2= new EmployeesTest("Carla");
+        EmployeesTest Employee3= new EmployeesTest("Antonio");
+        EmployeesTest Employee4= new EmployeesTest("Roberto");
       //Class Name | Object Name|OperatorNew|CONSTRUCTOR|Parameters
       //              Instance
 
@@ -14,6 +16,9 @@ public class TestConstant {
 
         System.out.println(Employee1.returnEmployeeData());
         System.out.println(Employee2.returnEmployeeData());
+        System.out.println(Employee3.returnEmployeeData());
+        System.out.println(Employee4.returnEmployeeData());
+
 
     }
 
@@ -22,6 +27,8 @@ public class TestConstant {
         public EmployeesTest(String nam){       // Step 4: Create Constructor Method
             name=nam;
             jobSection="Administration";
+            ID=NextID;
+            NextID++;
         }
 
                                                 // Step 5: Build Setters and Getters
@@ -31,12 +38,15 @@ public class TestConstant {
 
 
         public String returnEmployeeData(){         // GETTER
-            return "The name is: " + name + " and the section is: " + jobSection;
+            return "The name is: " + name + ", the job section is: " + jobSection +
+                    " and the ID=" + ID;
         }
 
         private final String name;      // CONSTANT
         private String jobSection;      // Step 3: Create instance fields or class fields
 
+        private final int ID;
+        private static int NextID=1;        // STATIC METHOD
     }
 
 }
