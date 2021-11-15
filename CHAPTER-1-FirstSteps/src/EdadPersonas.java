1b39c80b2b95abc993381770368ed80ecc65fc00
Erick Arias <erickarias1999@hotmail.com>
2021-11-15 16:59:21 -0500

    CHAPTER 1, First Steps, Edad Personas, Final Work, Java Course Ministry of Labor, Ecuador

---
+++ CHAPTER-1-FirstSteps/src/EdadPersonas.java
@@ -0,0 +1,43 @@
+/*
+    Nombre: Erick Roberto Arias Sánchez
+    CI: 1725044000
+    Email: erickarias1999@hotmail.com
+ */
+
+
+import java.util.Scanner;
+
+public class EdadPersonas {
+    public static void main(String[] args) {
+
+        String[] persona=new String[5];
+        int [] edad=new int[5];
+
+        // Inicializar variables
+        int menorEdad=100;
+        String personaMenor="";
+
+        Scanner dataEntry=new Scanner(System.in);
+
+        // Leer los nombres de las 5 personas
+        for(int i=0; i<5; i++){
+            System.out.println("Ingresa Nombre [" + (i+1) + "]: ");
+            persona[i]=dataEntry.nextLine();
+        }
+
+        // Leer las edades de las 5 personas
+        for(int i=0; i<5; i++){
+            System.out.println("Ingresa Edad de " + persona[i] + ": ");
+            edad[i]=dataEntry.nextInt();
+
+            // Condicional para seleccionar a la persona con menor edad
+            if(edad[i]<menorEdad){
+                menorEdad=edad[i];
+                personaMenor=persona[i];
+            }
+        }
+
+        System.out.println("\n" + personaMenor + " es la persona menor con "
+                + menorEdad + " años!");
+    }
+}
