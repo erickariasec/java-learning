773dbd594a9a22a954444d0b676643d444d42551
Erick Arias <erickarias1999@hotmail.com>
2021-11-22 12:56:10 -0500

    CHAPTER 1, POO, My Practices, Person The-Object-Oriented-Thought-Process (TOOTP) Example Class

---
+++ CHAPTER-1-FirstSteps/src/poo/mypractices/PersonTOOTPExampleClass.java
@@ -0,0 +1,24 @@
+package poo.mypractices;
+
+public class PersonTOOTPExampleClass {
+
+    //Attributes
+    private String name;
+    private String address;
+
+    //Methods
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        name = n;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+    public void setAddress(String adr){
+        address = adr;
+    }
+
+}
