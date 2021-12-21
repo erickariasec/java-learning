baa3b561ce40ad35a2ac6543d330f2a18df0707c
Erick Arias <erickarias1999@hotmail.com>
2021-12-21 09:58:38 -0500

    CHAPTER 1, POO, The Object Oriented Thought Process, Shape Use TOOTP, Book Example Commented

---
+++ CHAPTER-1-FirstSteps/src/poo/theoothoughtprocess/ShapeUseTOOTP.java
@@ -0,0 +1,10 @@
+package poo.theoothoughtprocess;
+
+/*public class ShapeUseTOOTP {
+
+    CircleTOOTP circle = new CircleTOOTP(5);
+    RectangleTOOTP rectangle = new RectangleTOOTP(4, 5);
+
+    stack.push(circle);
+    stack.push(rectangle);
+}*/
