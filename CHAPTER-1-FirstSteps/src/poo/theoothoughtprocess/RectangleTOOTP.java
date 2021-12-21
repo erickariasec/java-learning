230c8671ae659eb0c64d0d92cd7d2693174d8b72
Erick Arias <erickarias1999@hotmail.com>
2021-12-21 09:41:39 -0500

    CHAPTER 1, POO, The Object Oriented Thought Process, Rectangle TOOTP, Book Example

---
+++ CHAPTER-1-FirstSteps/src/poo/theoothoughtprocess/RectangleTOOTP.java
@@ -0,0 +1,19 @@
+package poo.theoothoughtprocess;
+
+public abstract class RectangleTOOTP extends ShapeTOOTP{
+
+    double length;
+    double width;
+
+    public RectangleTOOTP(double l, double w){
+        length = l;
+        width = w;
+    }
+
+    public double getArea() {
+        //area = length*width;
+        setArea(length*width);
+        return (area);
+    }
+
+}
