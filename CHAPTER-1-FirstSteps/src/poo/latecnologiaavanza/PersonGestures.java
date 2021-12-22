0747c186bcc4da2e32daf2666b89f41c801c07e6
Erick Arias <erickarias1999@hotmail.com>
2021-12-22 11:07:14 -0500

    CHAPTER 1, POO, La Tecnología Avanza - Youtube, Person Gestures, Stablish Greet and Say Goodbye Methods

---
+++ CHAPTER-1-FirstSteps/src/poo/latecnologiaavanza/PersonGestures.java
@@ -0,0 +1,13 @@
+package poo.latecnologiaavanza;
+
+public class PersonGestures {
+
+    public void greet(){
+        System.out.println("Hello, my name is Erick");
+    }
+
+    public void sayGoodbye(){
+        System.out.println("Bye, see you later!");
+    }
+
+}
