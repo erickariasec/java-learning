302e19ae0e5ab69c4389eaad8d94885a04a22338
Erick Arias <erickarias1999@hotmail.com>
2021-12-22 12:25:49 -0500

    CHAPTER 1, POO, La Tecnología Avanza - Youtube, Math Operations Use, Creating the object Solver, Calling the Main Method Show Menu

---
+++ CHAPTER-1-FirstSteps/src/poo/latecnologiaavanza/MathOperationsUse.java
@@ -0,0 +1,12 @@
+package poo.latecnologiaavanza;
+
+public class MathOperationsUse {
+
+    public static void main(String[] args) {
+
+        MathOperations solver = new MathOperations();
+        solver.showMenu();
+
+    }
+
+}
