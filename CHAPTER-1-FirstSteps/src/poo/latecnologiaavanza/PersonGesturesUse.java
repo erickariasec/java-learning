0d9cebefa9a7521523471c7cae0797c72c0b7c96
Erick Arias <erickarias1999@hotmail.com>
2021-12-22 11:11:47 -0500

    CHAPTER 1, POO, La Tecnología Avanza - Youtube, Person Gestures Use, Creating an object and Calling Methods from Person Gestures Class

---
+++ CHAPTER-1-FirstSteps/src/poo/latecnologiaavanza/PersonGesturesUse.java
@@ -0,0 +1,15 @@
+package poo.latecnologiaavanza;
+
+public class PersonGesturesUse {
+
+    public static void main(String[] args) {
+
+        // Creation of the object / Instantiating the Class
+        PersonGestures basicPersonGestures = new PersonGestures();
+
+        // Calling Methods
+        basicPersonGestures.greet();
+        basicPersonGestures.sayGoodbye();
+
+    }
+}
