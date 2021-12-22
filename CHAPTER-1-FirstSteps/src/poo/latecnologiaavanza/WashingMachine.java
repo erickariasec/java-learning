19fa54fddfc5a4ec8195397182c30b96d19ebfbe
Erick Arias <erickarias1999@hotmail.com>
2021-12-22 10:10:14 -0500

    CHAPTER 1, POO, La Tecnología Avanza - Youtube, Creation of Class Washing Machine with its attributes

---
+++ CHAPTER-1-FirstSteps/src/poo/latecnologiaavanza/WashingMachine.java
@@ -0,0 +1,11 @@
+package poo.latecnologiaavanza;
+
+public class WashingMachine {
+
+    // Create the attributes
+    int serialNumber;
+    String brand;
+    String color;
+    double price;
+
+}
