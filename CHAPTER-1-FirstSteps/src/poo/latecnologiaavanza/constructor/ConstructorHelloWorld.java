a6fee94fb50f6839f0fd696a7a10a19dd6922142
Erick Arias <erickarias1999@hotmail.com>
2021-12-23 08:39:22 -0500

    CHAPTER 1, POO, La Tecnología Avanza - Youtube, Constructor, Constructor Hello World, One way to create Constructor Method

---
+++ CHAPTER-1-FirstSteps/src/poo/latecnologiaavanza/constructor/ConstructorHelloWorld.java
@@ -0,0 +1,16 @@
+package poo.latecnologiaavanza.constructor;
+
+public class ConstructorHelloWorld {
+
+    // This is the Constructor Method
+    public ConstructorHelloWorld(){
+
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+    }
+
+}
