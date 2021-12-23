992c9fd54ebbd43a6017ad4260bb8b943a33bc02
Erick Arias <erickarias1999@hotmail.com>
2021-12-23 08:40:20 -0500

    CHAPTER 1, POO, La Tecnología Avanza - Youtube, Constructor, Constructor Hello World Use, Execution of Constructor Method

---
+++ CHAPTER-1-FirstSteps/src/poo/latecnologiaavanza/constructor/ConstructorHelloWorldUse.java
@@ -0,0 +1,11 @@
+package poo.latecnologiaavanza.constructor;
+
+public class ConstructorHelloWorldUse {
+
+    public static void main(String[] args) {
+
+        // Executes what is inside the Constructor Method
+        ConstructorHelloWorld greetWorld = new ConstructorHelloWorld();
+
+    }
+}
