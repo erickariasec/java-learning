11f311fb6895e18fb811d9a0ab661abc4eb4c677
Erick Arias <erickarias1999@hotmail.com>
2021-12-23 08:57:24 -0500

    CHAPTER 1, POO, La Tecnología Avanza - Youtube, Constructor, Constructor Person Use, Executing the Constructor Method

---
+++ CHAPTER-1-FirstSteps/src/poo/latecnologiaavanza/constructor/ConstructorPersonUse.java
@@ -0,0 +1,11 @@
+package poo.latecnologiaavanza.constructor;
+
+public class ConstructorPersonUse {
+    public static void main(String[] args) {
+
+        // Execute the Constructor Method
+        ConstructorPerson buildPerson1 = new ConstructorPerson("Erick", 22, 1.72);
+
+    }
+
+}
