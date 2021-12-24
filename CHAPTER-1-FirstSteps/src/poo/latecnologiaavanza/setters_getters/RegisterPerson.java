4880078e96cd34f29ac43b554d554983a6bcaa99
Erick Arias <erickarias1999@hotmail.com>
2021-12-23 20:27:56 -0500

    CHAPTER 1, POO, La Tecnología Avanza - Youtube, Setters and Getters, Register Person, Encapsulating Attributes, Adding Setters and Getters

---
+++ CHAPTER-1-FirstSteps/src/poo/latecnologiaavanza/setters_getters/RegisterPerson.java
@@ -0,0 +1,39 @@
+package poo.latecnologiaavanza.setters_getters;
+
+public class RegisterPerson {
+
+    private String name;
+    private int age;
+    private double height;
+
+    // SETTER for Name
+    public void setName(String name){
+        this.name = name;
+    }
+
+    // GETTER for Name
+    public String getName(){
+        return name;
+    }
+
+    // SETTER for Age
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    // GETTER for Age
+    public int getAge(){
+        return age;
+    }
+
+    // SETTER for Height
+    public void setHeight(double height){
+        this.height = height;
+    }
+
+    // GETTER for Height
+    public double getHeight(){
+        return height;
+    }
+
+}
