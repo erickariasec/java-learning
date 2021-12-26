521eb7186e0ed623f86f05f5bb5607cd9aacdaf0
Erick Arias <erickarias1999@hotmail.com>
2021-12-26 11:12:04 -0500

    CHAPTER 1, POO, La Tecnología Avanza - Youtube, Static Members, Person Static Use, Adding and Calling a Static GETTER in this Class

--- CHAPTER-1-FirstSteps/src/poo/latecnologiaavanza/static_members/PersonStaticUse.java
+++ CHAPTER-1-FirstSteps/src/poo/latecnologiaavanza/static_members/PersonStaticUse.java
@@ -1,8 +1,17 @@
 package poo.latecnologiaavanza.static_members;
 
-import poo.latecnologiaavanza.constant.PersonConstant;
 
 public class PersonStaticUse {
+
+    // This GETTER is only for this Class
+    public static int getAgeCalculation(int currentAge, int birthDate){
+        return currentAge - birthDate;
+    }
+
+    /*public void setGreet2(String name){
+        System.out.println("Hi, my name is " + name);
+    }*/
+
     public static void main(String[] args) {
 
         // I could call all attributes and methods STATIC that are in the Class not Object
@@ -10,9 +19,12 @@ public class PersonStaticUse {
         PersonStatic.setGreet();  // This is PersonStatic Class Method
         PersonStatic.setSpeaker();  // This is PersonStatic Class Method
 
+
         PersonStatic personStatic = new PersonStatic(); // Creating Object
         personStatic.setGoodbye(); // Calling the only method NOT static
 
-    }
+        int age = getAgeCalculation(2021, 1999); // Static GETTER for this Class Only
+        System.out.println("\n\nThe real age is " + age);
 
+    }
 }
