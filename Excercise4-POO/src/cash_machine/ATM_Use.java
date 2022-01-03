4ea289c6769aedb49b795ad5f31f032cbb16d430
Erick Arias <erickarias1999@hotmail.com>
2022-01-03 12:08:10 -0500

    Excercise 4 - POO, Cash Machine, ATM Use, Instantiating ATM Class, Calling Main Menu, Executable File

---
+++ Excercise4-POO/src/cash_machine/ATM_Use.java
@@ -0,0 +1,10 @@
+package cash_machine;
+
+public class ATM_Use {
+    public static void main(String[] args) {
+
+        ATM cashMachine = new ATM(); // Instantiating ATM Class
+        cashMachine.setMenu(); // Calling Main Menu
+
+    }
+}
