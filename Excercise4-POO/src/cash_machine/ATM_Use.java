5c9489ef2d0603b4a92726fcc84413d1203fb245
Erick Arias <erickarias1999@hotmail.com>
2022-01-03 18:26:00 -0500

    Excercise 4 - POO, Cash Machine, ATM Use, Calling Account Creator Method

--- Excercise4-POO/src/cash_machine/ATM_Use.java
+++ Excercise4-POO/src/cash_machine/ATM_Use.java
@@ -4,6 +4,7 @@ public class ATM_Use {
     public static void main(String[] args) {
 
         ATM cashMachine = new ATM(); // Instantiating ATM Class
+        cashMachine.setAccountCreator(); // Calling Account Creator
         cashMachine.setMenu(); // Calling Main Menu
 
     }
