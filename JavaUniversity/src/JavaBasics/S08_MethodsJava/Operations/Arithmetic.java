dd6b33ca5bf6d3fe57fe15ab34ddf46e62b6fb4a
Erick Arias <erickarias1999@hotmail.com>
2022-01-19 23:19:39 -0500

    Java University, Java Basics, S08 - Methods Java, Operations, Arithmetic, Adding Attributes and Method sum

---
+++ JavaUniversity/src/JavaBasics/S08_MethodsJava/Operations/Arithmetic.java
@@ -0,0 +1,15 @@
+package JavaBasics.S08_MethodsJava.Operations;
+
+public class Arithmetic {
+
+    // Class Attributes
+    int a;
+    int b;
+
+    // Method to sum
+    public void sum(){
+        int result = a + b;
+        System.out.println("The result of the sum is: " + result);
+    }
+
+}
