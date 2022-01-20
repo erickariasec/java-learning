fcbb09740f7cc3761b409964930a0c619c47a469
Erick Arias <erickarias1999@hotmail.com>
2022-01-20 10:17:22 -0500

    Java University, Java Basics, S10 - Variable Scope Java, Operations, Arithmetic Use, Creating Local Variables that are not being used, Creating Method that is outside of Main and Calling this Method to be inside of Main

--- JavaUniversity/src/JavaBasics/S10_VariableScopeJava/Operations/ArithmeticUse.java
+++ JavaUniversity/src/JavaBasics/S10_VariableScopeJava/Operations/ArithmeticUse.java
@@ -3,6 +3,11 @@ package JavaBasics.S10_VariableScopeJava.Operations;
 public class ArithmeticUse {
     public static void main(String[] args) {
 
+        // Local Variables
+        int a = 10;
+        int b = 2;
+        myMethod(); // Calling Method that is outside of main
+
         // Instantiating Arithmetic Class (Creating Object arithmetic1)
         Arithmetic arithmetic1 = new Arithmetic();
         System.out.println("arithmetic1 a: " + arithmetic1.a);  // Print 0 by default
@@ -12,5 +17,11 @@ public class ArithmeticUse {
         Arithmetic arithmetic2 = new Arithmetic(5, 8);  // Assigning Arguments
         System.out.println("arithmetic2 a: " + arithmetic2.a);
         System.out.println("arithmetic2 b: " + arithmetic2.b);
+    }
+
+    public static void myMethod(){  // It is important to be "static" for being used at main
+        // a = 10;
+        // Variable 'a' is OUT OF SCOPE where it was defined (main), so we could not use.
+        System.out.println("This is another method, which is outside of main");
     }
 }
