af5cb863e62bf7800de9d577172286abb6218391
Erick Arias <erickarias1999@hotmail.com>
2022-01-20 09:40:46 -0500

    Java University, Java Basics, S09 - Constructors Java, Operations, Arithmetic Use, Insantiating Arithmetic Class with Empty Constructor and Constructor with Parameters

---
+++ JavaUniversity/src/JavaBasics/S09_ConstructorsJava/Operations/ArithmeticUse.java
@@ -0,0 +1,16 @@
+package JavaBasics.S09_ConstructorsJava.Operations;
+
+public class ArithmeticUse {
+    public static void main(String[] args) {
+
+        // Instantiating Arithmetic Class (Creating Object arithmetic1)
+        Arithmetic arithmetic1 = new Arithmetic();
+        System.out.println("arithmetic1 a: " + arithmetic1.a);  // Print 0 by default
+        System.out.println("arithmetic1 b: " + arithmetic1.b);  // Print 0 by default
+
+        // Instantiating Arithmetic Class using Constructor Method with Parameters
+        Arithmetic arithmetic2 = new Arithmetic(5, 8);  // Assigning Arguments
+        System.out.println("arithmetic2 a: " + arithmetic2.a);
+        System.out.println("arithmetic2 b: " + arithmetic2.b);
+    }
+}
