cda8a31185209302eee17dcb34274929a3cd790f
Erick Arias <erickarias1999@hotmail.com>
2022-01-20 00:09:45 -0500

    Java University, Java Basics, S08 - Methods Java, Operations, Arithmetic Use, Calling sumPassingParametersMethod and Assigning Arguments

--- JavaUniversity/src/JavaBasics/S08_MethodsJava/Operations/ArithmeticUse.java
+++ JavaUniversity/src/JavaBasics/S08_MethodsJava/Operations/ArithmeticUse.java
@@ -12,8 +12,12 @@ public class ArithmeticUse {
 
         arithmetic1.sum(); // Prints what is inside of sum Method
 
-        int result = arithmetic1.sumWithReturn();
+        int result = arithmetic1.sumWithReturn(); // Calling sumWithReturn Method
         System.out.println("Result from ArithmeticUse: " + result);
+
+        // Calling sumPassingParameters Method
+        result = arithmetic1.sumEstablishingParameters(7, 4); // Assigning Arguments
+        System.out.println("The result of the sum after assigning arguments is: " + result);
 
     }
 }
