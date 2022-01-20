31a2976238df4b299b34bcaaed0cadb5f4390528
Erick Arias <erickarias1999@hotmail.com>
2022-01-20 10:02:35 -0500

    Java University, Java Basics, S10 - Variable Scope Java, Operations

---
+++ JavaUniversity/src/JavaBasics/S10_VariableScopeJava/Operations/Arithmetic.java
@@ -0,0 +1,41 @@
+package JavaBasics.S10_VariableScopeJava.Operations;
+
+public class Arithmetic {
+
+    // Class Attributes
+    int a;
+    int b;
+
+    // Empty Constructor Method
+    public Arithmetic(){
+        System.out.println("Executing Empty Constructor"); // It will be printed when instantiate this Class
+    }
+
+    // Constructor Method with Parameters
+    public Arithmetic(int a, int b){
+        this.a = a;
+        this.b = b;
+        System.out.println("Executing Constructor after Assigning Arguments in ArithmeticUse");
+    }
+
+    // Method to sum
+    public void sum(){
+        int result = a + b;
+        System.out.println("The result of the sum is: " + result);
+    }
+
+    // Method to sum with integer return
+    public int sumWithReturn(){
+        return a + b;
+    }
+
+    // Method to sum establishing parameters
+    public int sumEstablishingParameters(int a, int b){
+        /* "this" refers to an attribute of a Class */
+        this.a = a;     // Parameter 'a' is assigned to Attribute 'a'(this.a)
+        this.b = b;     // Parameter 'b' is assigned to Attribute 'b'(this.b)
+        //return a + b;
+        return sumWithReturn();  // Calls the logic of sumWithReturn Method
+    }
+
+}
