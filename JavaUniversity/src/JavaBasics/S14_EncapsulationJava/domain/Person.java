e6fa07c1d32dc15ee93cb3aa8541e06c9b1a30b5
Erick Arias <erickarias1999@hotmail.com>
2022-01-24 10:17:42 -0500

    Java University, Java Basics, S14 - Encapsulation Java, domain, Person, Encapsulating Attributes by using private word, Adding Getters and Setters for each Attribute

---
+++ JavaUniversity/src/JavaBasics/S14_EncapsulationJava/domain/Person.java
@@ -0,0 +1,37 @@
+package JavaBasics.S14_EncapsulationJava.domain;
+
+public class Person {
+    private String name;    // ENCAPSULATING ATTRIBUTES by using private
+    private double salary;
+    private boolean removed;
+
+    public Person(String name, double salary, boolean removed){
+        this.name = name;
+        this.salary = salary;
+        this.removed = removed;
+    }
+
+    public String getName(){  // GETTER
+        return this.name;
+    }
+
+    public void setName(String name){   // SETTER
+        this.name = name;
+    }
+
+    public double getSalary(){  // GETTER
+        return this.salary;
+    }
+
+    public void setSalary(double salary){   // SETTER
+        this.salary = salary;
+    }
+
+    public boolean isRemoved(){     // GETTER for boolean
+        return this.removed;
+    }
+
+    public void setRemoved(boolean removed){    // SETTER
+        this.removed = removed;
+    }
+}
