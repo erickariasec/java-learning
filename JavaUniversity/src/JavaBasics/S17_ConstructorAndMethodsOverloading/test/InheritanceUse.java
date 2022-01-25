4cd69ce8d9c06ec8b1b463e839c0c57376e8ad08
Erick Arias <erickarias1999@hotmail.com>
2022-01-25 17:45:15 -0500

    Java University, Java Basics, S17 - Constructor and Methods Overloading, test, Inheritance Use, Verify if Employee Objects is functioning after doing some modifications at Employee Class

--- JavaUniversity/src/JavaBasics/S17_ConstructorAndMethodsOverloading/test/InheritanceUse.java
+++ JavaUniversity/src/JavaBasics/S17_ConstructorAndMethodsOverloading/test/InheritanceUse.java
@@ -10,12 +10,12 @@ public class InheritanceUse {
         Employee employee1 = new Employee("Nicole", 5000.00); // Creating Object employee1 with these arguments
         System.out.println("employee1 = " + employee1);
 
-        System.out.println("\n\n");
-        // Instantiate Client Class                                                          // Prints actual date
-        Client client1 = new Client("Carla", 'F', 25, "LongStreet Avenue", new Date(), true);
-        System.out.println("client1 = " + client1);
-        // Prints actual date
-        Client client2 = new Client("Santiago", 'M', 23, "Maldonado Avenue", new Date(), false);
-        System.out.println("client2 = " + client2);
+//        System.out.println("\n\n");
+//        // Instantiate Client Class                                                          // Prints actual date
+//        Client client1 = new Client("Carla", 'F', 25, "LongStreet Avenue", new Date(), true);
+//        System.out.println("client1 = " + client1);
+//        // Prints actual date
+//        Client client2 = new Client("Santiago", 'M', 23, "Maldonado Avenue", new Date(), false);
+//        System.out.println("client2 = " + client2);
     }
 }
