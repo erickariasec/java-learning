c7cd1f45b56c124edf3a888cd9c10c7393b5e2bf
Erick Arias <erickarias1999@hotmail.com>
2022-01-25 11:36:59 -0500

    Java University, Java Basics, S16 - Inheritance Java, test, Inheritance Use, Creating object passing 2 Arguments

--- JavaUniversity/src/JavaBasics/S16_InheritanceJava/test/InheritanceUse.java
+++ JavaUniversity/src/JavaBasics/S16_InheritanceJava/test/InheritanceUse.java
@@ -4,8 +4,7 @@ import JavaBasics.S16_InheritanceJava.domain.Employee;
 
 public class InheritanceUse {
     public static void main(String[] args) {
-        Employee employee1 = new Employee();    // It will receive toString structure from Class Person
-        System.out.println("employee1 = " + employee1); // It will print toString structure with null information
-                                                        // because Employee is empty
+        Employee employee1 = new Employee("Nicole", 5000.00); // Creating Object employee1 with these arguments
+        System.out.println("employee1 = " + employee1);
     }
 }
