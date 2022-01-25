db44fbdfddaf5065de3eb79754d1030278c2c16d
Erick Arias <erickarias1999@hotmail.com>
2022-01-25 09:29:40 -0500

    Java University, Java Basics, S16 - Inheritance Java, domain, Employee, Class Employee extends Person (Inheritance), Class Employee is empty

---
+++ JavaUniversity/src/JavaBasics/S16_InheritanceJava/domain/Employee.java
@@ -0,0 +1,6 @@
+package JavaBasics.S16_InheritanceJava.domain;
+
+public class Employee extends Person{   // When I create an object at main (InheritanceUse) Although it
+                                        // is empty, it will send null information with structure of toString
+                                        // from Person parent Class
+}
