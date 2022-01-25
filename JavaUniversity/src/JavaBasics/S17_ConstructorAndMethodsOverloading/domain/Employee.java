67db37cb152128405623ef22c9ff68989e41653c
Erick Arias <erickarias1999@hotmail.com>
2022-01-25 17:11:33 -0500

    Java University, Java Basics, S17 - Constructor and Methods Overloading, domain, Employee

---
+++ JavaUniversity/src/JavaBasics/S17_ConstructorAndMethodsOverloading/domain/Employee.java
@@ -0,0 +1,35 @@
+package JavaBasics.S17_ConstructorAndMethodsOverloading.domain;
+
+public class Employee extends Person{   // Employee Class is child of Person (Person is Parent Class)
+    private int idEmployee;
+    private double salary;
+    private static int counterEmployee; // It must be static
+
+    public Employee(String name, double salary){    // Constructor with 2 Parameters
+        super(name);    // Calls constructor from Parent Class (Initialize)
+        this.idEmployee = ++JavaBasics.S17_ConstructorAndMethodsOverloading.domain.Employee.counterEmployee;   // Because of it is static, it will increment in each new object
+        this.salary = salary;
+    }
+
+    public int getIdEmployee() {    // GETTER
+        return idEmployee;
+    }
+
+    public double getSalary() {     // GETTER
+        return salary;
+    }
+
+    public void setSalary(double salary) {      // SETTER
+        this.salary = salary;
+    }
+
+    @Override       // Here we are overriding toString from Person Parent Class
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Employee{"); // StringBuilder is mutable, so we can change it
+        sb.append("idEmployee=").append(idEmployee);
+        sb.append(", salary=").append(salary);
+        sb.append(", ").append(super.toString());
+        sb.append('}');
+        return sb.toString();
+    }
+}
