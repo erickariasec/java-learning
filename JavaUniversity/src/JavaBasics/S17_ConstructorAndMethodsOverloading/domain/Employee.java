e8a5c2efde29cf7fe057bac0cf6f8b7717a8d7c3
Erick Arias <erickarias1999@hotmail.com>
2022-01-25 17:44:01 -0500

    Java University, Java Basics, S17 - Constructor and Methods Overloading, domain, Employee, Overload Constructor by adding another Constructor in the same Class, Using this( ) to call a constructor Method inside another Constructor Method from the same Class, Calling protected attribute from Person Class

--- JavaUniversity/src/JavaBasics/S17_ConstructorAndMethodsOverloading/domain/Employee.java
+++ JavaUniversity/src/JavaBasics/S17_ConstructorAndMethodsOverloading/domain/Employee.java
@@ -5,9 +5,16 @@ public class Employee extends Person{   // Employee Class is child of Person (Pe
     private double salary;
     private static int counterEmployee; // It must be static
 
-    public Employee(String name, double salary){    // Constructor with 2 Parameters
-        super(name);    // Calls constructor from Parent Class (Initialize)
+    // Overload Constructor is when there is more than one Constructor in one Class
+
+    public Employee(){
         this.idEmployee = ++Employee.counterEmployee;   // Because of it is static, it will increment in each new object
+    }
+
+    public Employee(String name, double salary){    // Constructor with 2 Parameters
+        //super(name);    // Calls constructor from Parent Class (Initialize) // super must be the first line
+        this(); // Refers to Empty Employee Constructor  // We could not use this() and super(), only one of them
+        this.name = name;   // We could do this because attribute name at Person Class is protected
         this.salary = salary;
     }
 
