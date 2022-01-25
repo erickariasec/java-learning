8a09485aab38e4e92d6bcd60eb5fb6de01896459
Erick Arias <erickarias1999@hotmail.com>
2022-01-25 13:09:03 -0500

    Java University, Java Basics, S16 - Inheritance Java, domain, Client, Practicing Concepts, Client extends Person(Parent Class), Creating Constructor with Parameters from Client Class and Person Class, Setters and Getters, Method toString Overrides Person Method toString but calling information from parent Class using super

---
+++ JavaUniversity/src/JavaBasics/S16_InheritanceJava/domain/Client.java
@@ -0,0 +1,48 @@
+package JavaBasics.S16_InheritanceJava.domain;
+
+import java.util.Date;
+
+public class Client extends Person{
+    private int idClient;
+    private Date registrationDate;
+    private boolean vip;
+    private static int counterClient;
+
+    public Client(String name, char genre, int age, String address, Date registrationDate, boolean vip){
+        super(name, genre, age, address);   // super must be always the first line for inheritance - It calls Parent Class Constructor
+        idClient = ++Client.counterClient;
+        this.registrationDate = registrationDate;
+        this.vip = vip;
+    }
+
+    public int getIdClient(){   // GETTER
+        return idClient;
+    }
+
+    public Date getRegistrationDate(){  // GETTER
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {    // SETTER
+        this.registrationDate = registrationDate;
+    }
+
+    public boolean isVip(){     // GETTER for Boolean
+        return vip;
+    }
+
+    public void setVip(boolean vip){    // SETTER
+        this.vip = vip;
+    }
+
+    @Override   // Overrides toString Person Parent Class
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Client{");
+        sb.append("idClient=").append(idClient);
+        sb.append(", registrationDate=").append(registrationDate);
+        sb.append(", vip=").append(vip);
+        sb.append(", ").append(super.toString());   // It will call what is inside toString from Person Parent Class
+        sb.append('}');
+        return sb.toString();
+    }
+}
