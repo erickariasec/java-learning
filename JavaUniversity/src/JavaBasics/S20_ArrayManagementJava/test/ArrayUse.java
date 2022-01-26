f721a8fb215b79158996cefbe0c83d16f185652e
Erick Arias <erickarias1999@hotmail.com>
2022-01-26 12:16:13 -0500

    Java University, Java Basics, S20 - Array Management Java, test, Array Use, Assigning values to each position of the Array and Printing them

--- JavaUniversity/src/JavaBasics/S20_ArrayManagementJava/test/ArrayUse.java
+++ JavaUniversity/src/JavaBasics/S20_ArrayManagementJava/test/ArrayUse.java
@@ -2,7 +2,18 @@ package JavaBasics.S20_ArrayManagementJava.test;
 
 public class ArrayUse {
     public static void main(String[] args) {
+
+        // REVIEWING ARRAYS
+
         int[] ages = new int[3];    // Creating an int array of 3 elements
-        System.out.println("ages = " + ages);   // It will print the location
+        System.out.println("ages = " + ages);   // It will print the location in memory
+
+        ages[0] = 10;   // The first position of an Array is 0
+        ages[1] = 15;
+        ages[2] = 20;
+
+        System.out.println("ages at position 0 = " + ages[0]);
+        System.out.println("ages at position 1 = " + ages[1]);
+        System.out.println("ages at position 2 = " + ages[2]);
     }
 }
