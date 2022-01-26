0bb6ae3109a652b241df4ecd3b57dbdbc477d3b7
Erick Arias <erickarias1999@hotmail.com>
2022-01-26 12:08:06 -0500

    Java University, Java Basics, S20 - Array Management Java, test, Array Use, Creating an int Array of 3 elements and Printing the location in memory of the Array

---
+++ JavaUniversity/src/JavaBasics/S20_ArrayManagementJava/test/ArrayUse.java
@@ -0,0 +1,8 @@
+package JavaBasics.S20_ArrayManagementJava.test;
+
+public class ArrayUse {
+    public static void main(String[] args) {
+        int[] ages = new int[3];    // Creating an int array of 3 elements
+        System.out.println("ages = " + ages);   // It will print the location
+    }
+}
