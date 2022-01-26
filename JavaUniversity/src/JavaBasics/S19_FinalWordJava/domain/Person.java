f283c8ed922587ea6ab15ebdf0250c3c41a76d60
Erick Arias <erickarias1999@hotmail.com>
2022-01-26 11:33:46 -0500

    Java University, Java Basics, S19 - final Word Java, domain, Person, Generating Private Attribute and Adding its Getter and Setter Method

--- JavaUniversity/src/JavaBasics/S19_FinalWordJava/domain/Person.java
+++ JavaUniversity/src/JavaBasics/S19_FinalWordJava/domain/Person.java
@@ -4,7 +4,18 @@ public final class Person {   // Person can't inherit to Child Classes because i
 
     public final static int MY_CONSTANT = 1;
 
+    private String name;
+
     public void prints(){   // If this Method were final, we could not modify its behavior at Child Classes
         System.out.println("Method prints");
     }
+
+    public String getName(){    // GETTER
+        return name;
+    }
+
+    public void setName(String name){    // SETTER
+        this.name = name;
+    }
+
 }
