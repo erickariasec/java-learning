e067611d432a974d5a5362b1ad28006eca102085
Erick Arias <erickarias1999@hotmail.com>
2022-01-26 18:54:27 -0500

    Java University, Java Basics, S21 - Matrix Handling Java, test, Matrix Use, Explanation about how a Matrix is, Explanation about Two Dimensional Array, Lines and Columns

---
+++ JavaUniversity/src/JavaBasics/S21_MatrixHandlingJava/test/MatrixUse.java
@@ -0,0 +1,12 @@
+package JavaBasics.S21_MatrixHandlingJava.test;
+
+// MATRIX is an Array of Arrays
+
+public class MatrixUse {                        //      COLUMNS  COLUMNS
+    public static void main(String[] args) {    //      _________________
+                        //    x   y               LINES |_______|_______|
+                        // lines columns          LINES |_______|_______|
+        int[][] ages = new int[3][2];     //      LINES |_______|_______|
+
+    }
+}
