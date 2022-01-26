cf4e30f4b375e3e88a253e802f4f78ed8182e0a6
Erick Arias <erickarias1999@hotmail.com>
2022-01-26 16:18:46 -0500

    Java University, Java Basics, S20 - Array Management Java, test, Array Object Use, Printing what is inside of the array using bucle for

--- JavaUniversity/src/JavaBasics/S20_ArrayManagementJava/test/ArrayObjectUse.java
+++ JavaUniversity/src/JavaBasics/S20_ArrayManagementJava/test/ArrayObjectUse.java
@@ -12,5 +12,12 @@ public class ArrayObjectUse {
 
         System.out.println("persons at position 0 = " + persons[0]);    // Use toString to show correct format
         System.out.println("persons at position 1 = " + persons[1]);    // else it will print only memory location
+
+        System.out.println("\n\n");
+
+        // DOING THE SAME WITH BUCLE FOR
+        for(int i=0; i < persons.length; i++){
+            System.out.println("persons at position " + i + " = " + persons[i]);
+        }
     }
 }
