08abc1937f3fd2b9d781e4d9cab2bc41a1d680c1
Erick Arias <erickarias1999@hotmail.com>
2022-01-26 11:34:51 -0500

    Java University, Java Basics, S19 - final Word Java, test, Final Use, Instantiating final Person Class

--- JavaUniversity/src/JavaBasics/S19_FinalWordJava/test/FinalUse.java
+++ JavaUniversity/src/JavaBasics/S19_FinalWordJava/test/FinalUse.java
@@ -1,6 +1,7 @@
 package JavaBasics.S19_FinalWordJava.test;
 
 import JavaBasics.S19_FinalWordJava.domain.Person;
+import jdk.nashorn.internal.runtime.JSErrorType;
 
 public class FinalUse {
     public static void main(String[] args) {
@@ -11,5 +12,16 @@ public class FinalUse {
         System.out.println("MY_CONSTANT = " + Person.MY_CONSTANT); // Calling directly because MY_CONSTANT is static
                                                 // static is part of the Class not the Object
                                                 // We can't modify this because is final also
+
+        final Person person1 = new Person();    // Instantiating final Person Class (final Object)
+        // person1 = new Person();  // This is not possible because is final
+        person1.setName("Lisbeth");
+        System.out.println("person1 name: " + person1.getName());
+        person1.setName("Tatiana");
+        System.out.println("person1 name modified: " + person1.getName());
+
+        final Person person2 = new Person();    // final Object person2
+        person2.setName("Eddy");
+        System.out.println("person2 name: " + person2.getName());
     }
 }
