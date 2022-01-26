5771e1ca4b4723999b68453e1debbc168128a7d4
Erick Arias <erickarias1999@hotmail.com>
2022-01-26 10:07:04 -0500

    Java University, Java Basics, S18 - Packages Java, ec, com, erickarias, Utilities, Creating Method to print a String

---
+++ JavaUniversity/src/JavaBasics/S18_PackagesJava/ec/com/erickarias/Utilities.java
@@ -0,0 +1,9 @@
+package JavaBasics.S18_PackagesJava.ec.com.erickarias;
+
+public class Utilities {
+
+    public static void prints(String s){    // Method to print a String
+        System.out.println("s = " + s);
+    }
+
+}
