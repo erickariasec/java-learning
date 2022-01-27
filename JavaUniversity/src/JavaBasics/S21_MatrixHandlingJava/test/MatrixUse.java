f6bc4b9acbc9cfc513f0426771072c7dab82b21a
Erick Arias <erickarias1999@hotmail.com>
2022-01-27 09:48:43 -0500

    Java University, Java Basics, S21 - Matriz Handling Java, test, Matrix Use, Complete Guide about how to print Arrays using Bucle for

--- JavaUniversity/src/JavaBasics/S21_MatrixHandlingJava/test/MatrixUse.java
+++ JavaUniversity/src/JavaBasics/S21_MatrixHandlingJava/test/MatrixUse.java
@@ -24,5 +24,52 @@ public class MatrixUse {                        //      COLUMNS  COLUMNS
         System.out.println("ages at position [2][0] = " + ages[2][0]);
         System.out.println("ages at position [2][1] = " + ages[2][1]);
 
+        System.out.println("\n\n");
+
+        /* ----> DEBUG TO SEE WHAT IS HAPPENING INSIDE BUCLE FOR <---- */
+
+        // DOING THE SAME WITH BUCLE FOR
+        for(int line = 0; line < ages.length; line++){
+            for(int column = 0; column < ages[line].length; column++){
+                System.out.println("ages at position [" + line + "][" + column + "] = " + ages[line][column]);
+            }
+        }
+
+        System.out.println("\n");
+
+        /* ----> DEBUG TO SEE WHAT IS HAPPENING INSIDE BUCLE FOR <---- */
+
+        // ANOTHER WAY USING BUCLE FOR
+        for(int line = 0; line < ages.length; line++){
+            System.out.println();
+            for(int column = 0; column < ages[line].length; column++){
+                System.out.print(ages[line][column] + " ");
+            }
+        }
+
+        System.out.println("\n");
+
+        /* ----> DEBUG TO SEE WHAT IS HAPPENING INSIDE BUCLE FOR <---- */
+
+        // ANOTHER WAY USING BUCLE FOR WITH VARIABLES i AND j
+        for(int i = 0; i < ages.length; i++){
+            System.out.println();
+            for(int j = 0; j < ages[i].length; j++){
+                System.out.print(ages[i][j] + " ");
+            }
+        }
+
+        System.out.println("\n");
+
+        /* ----> DEBUG TO SEE WHAT IS HAPPENING INSIDE BUCLE FOR <---- */
+
+        // ANOTHER WAY USING BUCLE FOR SHOWING POSITION
+        for(int line = 0; line < ages.length; line++){
+            System.out.println();
+            for(int column = 0; column < ages[line].length; column++){
+                System.out.print("ages at position [" + line + "][" + column + "] = " + ages[line][column] + "\t\t");
+            }
+        }
+
     }
 }
