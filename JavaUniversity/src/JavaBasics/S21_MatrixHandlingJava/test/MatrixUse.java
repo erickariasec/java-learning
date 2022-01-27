ca2717ed74da2119895e039b7db7a07b8b2c378a
Erick Arias <erickarias1999@hotmail.com>
2022-01-27 08:56:38 -0500

    Java University, Java Basics, S21 - Matriz Handling Java, test, Matrix Use, Assigning values to different positions in Two Dimensional Array

--- JavaUniversity/src/JavaBasics/S21_MatrixHandlingJava/test/MatrixUse.java
+++ JavaUniversity/src/JavaBasics/S21_MatrixHandlingJava/test/MatrixUse.java
@@ -8,5 +8,21 @@ public class MatrixUse {                        //      COLUMNS  COLUMNS
                         // lines columns          LINES |_______|_______|
         int[][] ages = new int[3][2];     //      LINES |_______|_______|
 
+        System.out.println("ages = " + ages + "\n\n"); // It will show ages memory location
+
+        ages[0][0] = 5;
+        ages[0][1] = 7;
+        ages[1][0] = 8;
+        ages[1][1] = 4;
+        ages[2][0] = 6;
+        ages[2][1] = 9;
+
+        System.out.println("ages at position [0][0] = " + ages[0][0]);
+        System.out.println("ages at position [0][1] = " + ages[0][1]);
+        System.out.println("ages at position [1][0] = " + ages[1][0]);
+        System.out.println("ages at position [1][1] = " + ages[1][1]);
+        System.out.println("ages at position [2][0] = " + ages[2][0]);
+        System.out.println("ages at position [2][1] = " + ages[2][1]);
+
     }
 }
