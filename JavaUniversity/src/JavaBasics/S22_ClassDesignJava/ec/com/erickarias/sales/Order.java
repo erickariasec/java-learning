51f82db2d01aa3fd0a6d24d539ef3d6ac0976b22
Erick Arias <erickarias1999@hotmail.com>
2022-01-29 23:42:07 -0500

    Java University, Java Basics, S22 - Class Design Java, ec, com, erickarias, sales, Order, Creating Order Class based on UML Diagram, Method to Calculate Total

--- JavaUniversity/src/JavaBasics/S22_ClassDesignJava/ec/com/erickarias/sales/Order.java
+++ JavaUniversity/src/JavaBasics/S22_ClassDesignJava/ec/com/erickarias/sales/Order.java
@@ -13,12 +13,22 @@ public class Order {
         this.products = new Product[Order.MAX_PRODUCTS];    // Initialize Products Array
     }
 
-    public void addProduct(Product product){    // Constructor to Add Product
+    public void addProduct(Product product){    // Method to Add Product
         if(productCounter < Order.MAX_PRODUCTS){    // Validate
             products[productCounter++] = product;
         } else {
             System.out.println("The maximum number of products has been exceeded: " + Order.MAX_PRODUCTS);
         }
+    }
+
+    public double calculateTotal(){     // Method to Calculate Total
+        double total = 0;
+        for (int i=0; i<this.productCounter; i++){
+//            Product product = this.products[i];
+//            total += product.getPrice();
+            total += this.products[i].getPrice(); // It does the same as the two last lines.
+        }
+        return total;
     }
 
 }
