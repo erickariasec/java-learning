add87b5a99a3574fbd73849f2a56552ef733bfd6
erickariasec <erickarias1999@hotmail.com>
2022-04-24 11:54:07 -0500

    ITI-Practices, Aritmetica

--- ITI-Practices/Artimetica/src/main/java/artimetica/PruebaAritmetica.java
+++ ITI-Practices/Artimetica/src/main/java/artimetica/PruebaAritmetica.java
@@ -9,17 +9,13 @@ import java.util.Scanner;
 public class PruebaAritmetica {
     public static void main(String[] args) {
         Aritmetica a = new Aritmetica();
-        
-        int num1;
-        int num2;
-        
         Scanner sc = new Scanner(System.in);
         
         System.out.println("Ingresa el primer numero: ");
-        num1 = sc.nextInt();
+        int num1 = sc.nextInt();
         
         System.out.println("Ingresa el segundo numero: ");
-        num2 = sc.nextInt();
+        int num2 = sc.nextInt();
         
         a.setNum1(num1);
         a.setNum2(num2);
