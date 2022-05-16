e08bf0c30f5da95c7ae508236054fd277f43197d
erickariasec <erickarias1999@hotmail.com>
2022-05-15 21:35:15 -0500

    ITI-Practices, Introduction, introduction, rent, RentAppUse, adding some explanation in comments

--- ITI-Practices/POO/Introduction/src/main/java/introduction/rent/RentAppUse.java
+++ ITI-Practices/POO/Introduction/src/main/java/introduction/rent/RentAppUse.java
@@ -2,21 +2,25 @@
 
 package introduction.rent;
 
-import java.util.ArrayList;
+import java.util.ArrayList; // Importamos el paquete que contiene el ArrayList
 
 public class RentAppUse {
     public static void main(String[] args) {
+        
+        // INSTANCIAMOS LAS CLASES
         Residence apartment1 = new Apartment("R. Stephenson S87-12 y J. Paez",
         13, "Quito", "Pichincha", "170102", 500.00);
         
         Residence house1 = new House("U. Jacobo S16-97 y T. West", "Cuenca", "Azuay",
         "121987", 800.00);
         
-        ArrayList<Residence> unidadesDeAlquiler = new ArrayList<>();
+        ArrayList<Residence> unidadesDeAlquiler = new ArrayList<>(); // Creación del ArrayList
         
+        // Añadimos objetos a nuestro ArrayList
         unidadesDeAlquiler.add(apartment1);
         unidadesDeAlquiler.add(house1);
         
+        // Dependiendo de la instancia se imprimirá uno u otro valor
         for(Residence unidad : unidadesDeAlquiler) {
             if(unidad instanceof Apartment) {
                 System.out.println("DEPARTAMENTO:");
@@ -24,7 +28,7 @@ public class RentAppUse {
                 System.out.println("CASA:");
             }
             
-            System.out.println(unidad);
+            System.out.println(unidad); // Gracias al Polimorfismo, cada unidad actúa de diferente manera
             System.out.printf("$%,.2f\n\n", unidad.primerPago());
         }
     }
