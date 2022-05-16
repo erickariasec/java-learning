922eb7c000ce3a7977c524697af83d0cd7198840
erickariasec <erickarias1999@hotmail.com>
2022-05-15 21:34:51 -0500

    ITI-Practices, Introduction, introduction, rent, House, adding some explanation in comments

--- ITI-Practices/POO/Introduction/src/main/java/introduction/rent/House.java
+++ ITI-Practices/POO/Introduction/src/main/java/introduction/rent/House.java
@@ -2,14 +2,17 @@
 
 package introduction.rent;
 
-public class House extends Residence {
+public class House extends Residence { // Aplicando Herencia
 
+    // ENCAPSULACIÓN
     private final double DEPOSITO_SEGURO = 200.00;
     
+    // MÉTODO CONSTRUCTOR
     public House(String direccion, String ciudad, String provincia, String codigoPostal, double rentaMensual) {
         super(direccion, ciudad, provincia, codigoPostal, rentaMensual);
     }
     
+    @Override
     public double primerPago(){
         return getRentaMensual() + IMPUESTO + DEPOSITO_SEGURO;
     }
